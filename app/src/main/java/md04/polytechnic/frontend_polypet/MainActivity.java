5656adcc87335ef48da9cff11ca85a5b6775375d
admin <dangthetien@gmail.com>
2024-07-17 21:28:28 +0700

    tien update activity_list_product + activity_main + fragmnet_favourist + fragment_home + fragment_mange + item_flash_sale + item_suggest _ BottomTabNav_Adapter + Mainactivity + MangeFragment + NotificationPagerAdapter

--- app/src/main/java/md04/polytechnic/frontend_polypet/MainActivity.java
+++ app/src/main/java/md04/polytechnic/frontend_polypet/MainActivity.java
@@ -1,5 +1,6 @@
 package md04.polytechnic.frontend_polypet;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import md04.polytechnic.frontend_polypet.adapter.BottomTabNav_Adapter;
+import md04.polytechnic.frontend_polypet.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomTabNav_Adapter adapter = new BottomTabNav_Adapter(this);
+        binding.viewPager.setAdapter(adapter);
+
+        binding.navigation.setOnItemSelectedListener(item -> {
+            int viewPagerPosition = 0; // Default position
+
+            if (item.getItemId() == R.id.btnHome) {
+                viewPagerPosition = 0;
+            } else if (item.getItemId() == R.id.btnFavourite) {
+                viewPagerPosition = 1;
+            } else if (item.getItemId() == R.id.btnNotification) {
+                viewPagerPosition = 2;
+            } else if (item.getItemId() == R.id.btnProfile) {
+                viewPagerPosition = 3;
+            }
+
+            binding.viewPager.setCurrentItem(viewPagerPosition, true);
+            return true;
+        });
+
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                // Update BottomNavigationView when ViewPager2 page changes
+                if (position == 0) {
+                    binding.navigation.setSelectedItemId(R.id.btnHome);
+                } else if (position == 1) {
+                    binding.navigation.setSelectedItemId(R.id.btnFavourite);
+                } else if (position == 2) {
+                    binding.navigation.setSelectedItemId(R.id.btnNotification);
+                } else if (position == 3) {
+                    binding.navigation.setSelectedItemId(R.id.btnProfile);
+                }
+            }
         });
     }
 }
